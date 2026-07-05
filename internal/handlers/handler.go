df19a99bf6efe4a3c02053b5e06503466c897abf
Gadzhimurad Rabadanov <g_rabadanov@baunanracing.ru>
2026-07-05 23:27:35 +0300

    add handler

---
+++ internal/handlers/handler.go
@@ -0,0 +1,22 @@
+package handlers
+package service
+
+import (
+	"net/http"
+	 "github.com/joho/godotenv"
+)
+
+type Server struct {
+	mux    *http.ServeMux
+	server http.Server
+}
+func CreateServe(){
+	http.ListenAndServe(":8080",nil)
+}
+func HandleRequest() {
+	http.HandlerFunc()
+}
+func main() {
+
+}
+
