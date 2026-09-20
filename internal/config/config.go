2008f127154281997ae613a9eb204c69b0edd53b
Gadzhimurad Rabadanov <g_rabadanov@baunanracing.ru>
2026-09-20 21:19:27 +0300

    Added loading function from env

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,29 @@
 package config
 
+import (
+	"log"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	ConcurrencyConfig
 	LinksConfig
 	AppConfig
+	UserConfig
+}
+
+func LoadFromEnv() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("can't load config settings from .env, error=%s", err)
+	}
+
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		log.Fatalf("can't parse config from environment, error=%s", err)
+	}
+
+	return cfg
 }
